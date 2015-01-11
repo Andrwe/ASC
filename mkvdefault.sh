125bbc460fe34985fd1857ad2a0c88862a0998b3
Andrwe Lord Weber <github@andrwe.org>
2015-01-11 01:07:56 +0100

    fix IFS handling & change to comma seperated list
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- mkvdefault.sh
+++ mkvdefault.sh
@@ -2,19 +2,24 @@
 
 if [ "$1" == "-h" -o -z "$1" ]
 then
-	echo "Script for changing default audio track of mkv to german track. Usage $0 \"film1.mkv\" \"film2.mkv\""
+	echo "Script for changing default audio track of mkv to german track. Usage $0 film1.mkv,film2.mkv"
 fi
 
+ifs="$IFS"
+IFS=","
 for f in $@
 do
 	rm=""
 	echo "$f"
 	tracks="$(mediainfo --Output="Audio;%Language/String3%:%ID%:%Default%\n" "$f")"
-	defid="$(echo "${track}" | grep Yes | cut -d':' -f2)"
-	gerid="$(echo "${track}" | grep -E "deu|ger|Deutsch|German|Deu|Ger" | cut -d':' -f2)"
+	defid="$(echo "${tracks}" | grep Yes | cut -d':' -f2)"
+	gerid="$(echo "${tracks}" | grep -E "deu|ger|Deutsch|German|Deu|Ger" | cut -d':' -f2)"
+	IFS="${ifs}"
 	for id in ${defid}
 	do
 		rm="${rm} -e track:${id} -s flag-default=0"
 	done
 	mkvpropedit ${rm} -e track:${gerid} -s flag-default=1 "$f"
+	IFS=","
 done
+IFS="$ifs"
