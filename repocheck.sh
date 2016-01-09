2733029d1cfec71a7883c5f7fe3b9f1f1d4946a5
Andrwe Lord Weber <github@andrwe.org>
2016-01-09 16:13:09 +0100

    added repo package check
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

---
+++ repocheck.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+######################################################################
+#
+#                  Author: Andrwe Lord Weber
+#                  Mail: lord-weber-andrwe<at>andrwe<dot>org
+#                  Version: 0.4
+#
+######################################################################
+
+######################################################################
+#                  script variables and functions
+######################################################################
+
+# Set name of the repository
+REPONAME="andrwe seiichiro"
+# Define pipe seperated list of packages to be skipped
+#   wildcards and regex can be used
+#   e.g.: BLACKLIST="aws-.*|actkbd"
+BLACKLIST="ruby-.*"
+
+# command to get aur version of a package
+#  - has to return only version string
+#  - is later called using "AURVERCMD pkg"
+AURVERCMD="cower --format=%v -i"
+
+# cleanup command for pkg db
+# for pkg in $(zcat andrwe.db.tar.gz | grep -a -A1 '%NAME%' | grep -v '%NAME%' | grep -v '\-\-' | sort -u);do ls "$pkg"* &>/dev/null || repo-remove andrwe.db.tar.gz "$pkg";done
+
+######################################################################
+#
+#                  No changes needed after this line.
+#
+######################################################################
+
+export LANG=C
+
+while read repo pkg pacver state; do
+  [[ ${pkg} =~ ${BLACKLIST} ]] && continue
+  if ! [[ ${pkg} =~ .*-(git|svn|cvs|hg) ]]; then
+    aurver="$(${AURVERCMD} "${pkg}" 2>/dev/null)"
+    if [[ -z "${aurver}" ]]; then
+      [[ DEBUG -gt 0 ]] && echo "no AUR version for ${pkg}"
+      continue
+    fi
+    pacver_num="${pacver//[-._]/}"
+    aurver_num="${aurver//[-._]/}"
+    if [[ ${pacver_num} =~ ^[0-9]+$ ]] && [[ ${aurver_num} =~ ^[0-9]+$ ]]; then
+      [[ ${pacver_num##0} -lt ${aurver_num##0} ]] && echo "${repo} ${pkg} ${aurver} cur: ${pacver}"
+      continue
+    fi
+    [[ "${pacver}" != "${aurver}" ]] && echo "${repo} ${pkg} ${aurver} cur: ${pacver}"
+  fi
+done <<<"$(pacman -Sl ${REPONAME})"
